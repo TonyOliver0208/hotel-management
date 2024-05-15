08245a54551af1a6e0b01335b75b978d6053e80e
TonyOliver0208 <nguyenphuoclong.0208@gmail.com>
2024-05-15 19:57:50 +0700

    final build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['lh3.googleusercontent.com', 'images.unsplash.com'],
+    },
+  };
+ 
+// module.exports = nextConfig;
 export default nextConfig;
+
